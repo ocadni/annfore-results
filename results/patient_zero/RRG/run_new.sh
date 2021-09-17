0b152232f0dfef4a34ad1cae0e910354a7ee6fa1
Fabio Mazza <fab4mazz@gmail.com>
2021-09-17 11:48:34 +0200

    Add results for the RRG

--- results/patient_zero/RRG/run_new.sh
+++ results/patient_zero/RRG/run_new.sh
@@ -16,16 +16,16 @@ t_limit=15
 #saving path
 #path_dir="/home/ibiazzo/nn_epidemics/results/patient_zero/RRG/data"
 #path_dir="../patient_zero/RRG/data"
-path_dir="$(pwd)/data/ann_new"
+path_dir="$(pwd)/data/"
 #python bin
 #python="/home/ibiazzo/miniconda3/bin/python3"
 python="python3"
 
 # nn parameters
 p_source=1e-5
-n_beta_steps=1000
-iter_marginals=50
-device="cuda:1"
+n_beta_steps=10000
+iter_marginals=100
+device="cuda:0"
 start_conf=0
 num_conf=10
 
@@ -37,13 +37,17 @@ a_min=0.01
 a_max=0.3
 a_step=0.01
 
+GEN_EPI="--type_graph $type_graph -T $t_limit -N $N -d $d --lambda $lambda --mu $mu"
+CONFS=" --num_conf $num_conf --start_conf $start_conf"
+
+
 SCRIPTFOLD="../../../scripts"
 mkdir -p $path_dir
 cd ../../../scripts/
 for seed in {0..9}
 do
-    $python ./nn_run_redo.py --type_graph $type_graph -T $t_limit -N $N -d $d --lambda $lambda --mu $mu --seed $seed --p_source $p_source --path_dir $path_dir --n_beta_steps $n_beta_steps --num_conf $num_conf --device $device --iter_marginals $iter_marginals --start_conf $start_conf --n_hidden_layers 4 --lay_less_deep
-    #$python ./sib_run_new.py --type_graph $type_graph -T $t_limit -N $N -d $d --lambda $lambda --mu $mu --seed $seed --path_dir $path_dir --num_conf $num_conf --start_conf $start_conf --p_source $p_source
+    #$python ./nn_run_redo.py $GEN_EPI $CONFS --seed $seed --p_source $p_source --path_dir $path_dir --n_beta_steps $n_beta_steps --device $device --iter_marginals $iter_marginals --lay_deep_eq
+    $python ./sib_run_new.py $GEN_EPI $CONFS --seed $seed --path_dir $path_dir"sib" --p_source $p_source
     #$python ./softmargin_run_multi.py --type_graph $type_graph -T $t_limit -N $N -d $d --lambda $lambda --mu $mu --seed $seed --path_dir $path_dir"/sm" --num_conf $num_conf --start_conf $start_conf --ncores $ncores --nsims_probs $nsims  --a_min $a_min --a_max $a_max --a_step $a_step --overwrite
     #done
 done
