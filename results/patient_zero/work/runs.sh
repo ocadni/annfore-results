216b7bf03fcedfd68e08869620959ad5afda4496
Fabio Mazza <fab4mazz@gmail.com>
2021-09-20 15:46:49 +0200

    Fix run script

--- results/patient_zero/work/runs.sh
+++ results/patient_zero/work/runs.sh
@@ -7,10 +7,11 @@ start_conf=0
 num_conf=50
 t_limit=12
 small_lambda_limit=0
-path_contacts="../patient_zero/work/work_13_contacts.npz"
+
+path_contacts="$(pwd)/work_13_contacts.npz"
 
 #saving path
-path_dir="../patient_zero/work/data"
+path_dir="$(pwd)/data/ann"
 
 #python bin
 python="python3"
@@ -30,17 +31,22 @@ a_step=0.01
 nsims="1_000_000 10_000_000 100_000_000"
 #nsims="1_000"
 
+GEN_EPI="--type_graph $type_graph -T $t_limit --mu $mu"
+CONFS=" --num_conf $num_conf --start_conf $start_conf"
+EXTRA_GEN="--path_contacts $path_contacts --small_lambda_limit $small_lambda_limit  --gamma $gamma"
 
-cd ../../script
+SCRIPTFOLD="../../../scripts"
+mkdir -p $path_dir
+cd ../../../scripts/
 
 #for seed in 1 2
-for seed in 2
+for seed in 1
 do
-    #python ./nn_run_redo.py --type_graph $type_graph -T $t_limit --gamma $gamma --mu $mu --seed $seed --p_source $p_source --path_dir $path_dir"/ann" --n_beta_steps $n_beta_steps --num_conf $num_conf --device $device --path_contacts $path_contacts --small_lambda_limit $small_lambda_limit --iter_marginals $iter_marginals --start_conf $start_conf
+    python ./nn_run_redo.py $GEN_EPI $CONFS $EXTRA_GEN --seed $seed --p_source $p_source --path_dir $path_dir --n_beta_steps $n_beta_steps --device $device --iter_marginals $iter_marginals --lay_less_deep
 
-    #python ./sib_run_new.py --type_graph $type_graph -T $t_limit --gamma $gamma --mu $mu --seed $seed --p_source $p_source --path_dir $path_dir"/sib" --num_conf $num_conf --path_contacts $path_contacts --small_lambda_limit $small_lambda_limit --start_conf $start_conf
+    #python ./sib_run_new.py $GEN_EPI $CONFS $EXTRA_GEN --seed $seed --p_source $p_source --path_dir $path_dir"/sib"
 
 
-    python ./softmargin_run_multi.py  --type_graph $type_graph -T $t_limit --gamma $gamma  --mu $mu --seed $seed  --path_dir $path_dir"/sm" --num_conf $num_conf --ncores $ncores --nsims_probs $nsims  --a_min $a_min --a_max $a_max --a_step $a_step --overwrite --path_contacts $path_contacts --small_lambda_limit $small_lambda_limit --start_conf $start_conf
+    #python ./softmargin_run_multi.py $GEN_EPI $CONFS $EXTRA_GEN --path_dir $path_dir"/sm" --ncores $ncores --nsims_probs $nsims  --a_min $a_min --a_max $a_max --a_step $a_step --overwrite
 
 done
