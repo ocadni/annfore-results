f884b0237942fccd3536cf4bb58fffaaf6a7e10b
Fabio Mazza <fab4mazz@gmail.com>
2021-10-03 20:08:19 +0200

    Update scripts for risk inference

--- results/risk_assessment/run_nn_RRG.sh
+++ results/risk_assessment/run_nn_RRG.sh
@@ -40,7 +40,7 @@ nsrc=1
 #gamma=1e-3
 #path_contacts="../patient_zero/work/work_13_contacts.npz"
 #small_lambda_limit=0
-path_dir="$(pwd)/ann/good_new/${type_graph}_${nsrc}src"
+path_dir="$(pwd)/ann/${type_graph}_${nsrc}src/3lpow1/"
 
 # nn parameters
 p_source=1e-2
@@ -53,7 +53,7 @@ num_samples=10000
 num_betas=10000
 
 
-SCRIPT="-u ../../results/script/nn_run_redo.py"
+SCRIPT="-u ../../scripts/nn_run_redo.py"
 #SCRIPT="-u ../results/script/nn_run_new.py"
 #init_name="psrc4_3rnd_pfinNew08_"
 #sparse observ
@@ -63,16 +63,16 @@ n_test_rnd=1
 delay_test_p="0. 0.4 0.5 0.6"
 
 #init_name="10kst10ks_${n_test_rnd}rnd_6lexp_"
-init_name="10kst10ks_lastobs_4lpow_psus07_psrc2_"
+init_name="10kst10ks_lastobs_psus07_psrc2_"
 
 
 GEN_GRAPH="--type_graph $type_graph -N $N -d $d -height $height -T $t_limit --lambda $lambda --mu $mu -p_edge $p_edge --n_sources $nsrc"
 
-EXTRA_FLAGS=" --init_name_file $init_name --path_dir $path_dir --sparse_obs_last"
-SPARSE_OBS="--sparse_obs --sparse_rnd_tests $n_test_rnd --pr_sympt $pr_sympt --delay_test_p $delay_test_p"
+EXTRA_FLAGS=" --init_name_file $init_name --path_dir $path_dir "
+SPARSE_OBS="--sparse_obs --sparse_rnd_tests $n_test_rnd --pr_sympt $pr_sympt --delay_test_p $delay_test_p --sparse_obs_last"
 TRAINING="--iter_marginals $iter_marginals --p_source $p_source --num_samples $num_samples --device $device --n_beta_steps $num_betas"
 #--p_rec_t0 $p_rec_t0 
-EXPER="--p_sus 0.7 --p_fin_bal --t_obs $t_limit --n_hidden_layers 4"
+EXPER="--p_sus 0.7 --p_fin_bal --t_obs $t_limit --n_hidden_layers 3 --lin_net_pow 1"
 
 num_conf=1
 st_conf=0
@@ -82,7 +82,7 @@ echo "NUM SAMPLES $num_samples"
 
 
 mkdir -p $path_dir
-for seed in $(seq 96 100)
+for seed in $(seq 10 15)
 do
     #if [ $seed -eq 45 ]; then
     #    continue
--- results/risk_assessment/run_nn_proximity.sh
+++ results/risk_assessment/run_nn_proximity.sh
@@ -31,7 +31,7 @@ nsrc=1
 #gamma=1e-3
 #path_contacts="../patient_zero/work/work_13_contacts.npz"
 #small_lambda_limit=0
-path_dir="$(pwd)/ann/${type_graph}_${nsrc}src/lastobs/"
+path_dir="$(pwd)/ann/${type_graph}_${nsrc}src/sparse_t/"
 
 # nn parameters
 p_source=1e-2
@@ -58,8 +58,8 @@ init_name="10kst10ks_3lpow1_psus07_psrc2_"
 
 GEN_GRAPH="--type_graph $type_graph -N $N -d $d -height $height -T $t_limit --lambda $lambda --mu $mu -p_edge $p_edge -scale $scale"
 
-EXTRA_FLAGS=" --init_name_file $init_name --path_dir $path_dir --sparse_obs_last"
-SPARSE_OBS="--sparse_obs --sparse_rnd_tests $n_test_rnd --pr_sympt $pr_sympt --delay_test_p $delay_test_p"
+EXTRA_FLAGS=" --init_name_file $init_name --path_dir $path_dir"
+SPARSE_OBS="--sparse_obs --sparse_rnd_tests $n_test_rnd --pr_sympt $pr_sympt --delay_test_p $delay_test_p" # --sparse_obs_last
 TRAINING="--iter_marginals $iter_marginals --p_source $p_source --num_samples $num_samples --device $device --n_beta_steps $num_betas"
 #--p_rec_t0 $p_rec_t0 
 EXPER="--p_sus 0.7 --p_fin_bal --t_obs $t_limit --n_hidden_layers 3 --lin_net_pow 1"
@@ -68,7 +68,7 @@ num_conf=1
 st_conf=0
 
 mkdir -p $path_dir
-for seed in $(seq 0 1)
+for seed in $(seq 0 30)
 do
     echo $seed
     n_conf=$(( $st_conf+$num_conf ))
