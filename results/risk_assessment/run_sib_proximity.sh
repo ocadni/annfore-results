3b665eb658cd6689adad4c31cfd0b06187cbf15e
Fabio Mazza <fab4mazz@gmail.com>
2021-10-14 17:04:16 +0200

    Update scripts for sib

--- results/risk_assessment/run_sib_proximity.sh
+++ results/risk_assessment/run_sib_proximity.sh
@@ -2,10 +2,8 @@
 <<EOF
 
 RUN NN RRG
-
-
 type_graph="RRG"
-seed=4
+seed=7
 N=100
 d=10
 height=3
@@ -14,36 +12,25 @@ mu=0.02
 t_limit=15
 p_edge=1
 
-type_graph="RRG"
-seed=7
+EOF
+
+type_graph="proximity"
+seed=1
 N=100
 d=10
 height=3
+#lambda=0.026
 lambda=0.03
 mu=0.
 t_limit=15
 p_edge=1
-nsrc=2
 
-EOF
-
-type_graph="RRG"
-N=100
-d=10
-height=3
-lambda=0.035
-mu=0.
-t_limit=15
-p_edge=1
+scale=2
 nsrc=1
 
-#num_conf=50
-#gamma=1e-3
-#path_contacts="../patient_zero/work/work_13_contacts.npz"
-#small_lambda_limit=0
+
 path_dir="$(pwd)/sib/${type_graph}_${nsrc}src"
 
-# nn parameters
 p_source=1e-2
 #step=1e-4
 p_sus=0.7
@@ -65,13 +52,13 @@ num_conf=1
 st_conf=0
 
 
-GEN_GRAPH="--type_graph $type_graph -N $N -d $d -height $height -T $t_limit --lambda $lambda --mu $mu -p_edge $p_edge --num_conf $num_conf --start_conf $st_conf  --n_sources $nsrc"
+GEN_GRAPH="--type_graph $type_graph -N $N -d $d -height $height -T $t_limit --lambda $lambda --mu $mu -p_edge $p_edge -scale $scale --n_sources $nsrc"
 
 EXTRA_GEN="--gamma $gamma --path_contacts $path_contacts --small_lambda_limit $small_lambda_limit"
 
 
-EXTRA_FLAGS=" --init_name_file $init_name --path_dir $path_dir --sparse_obs_last"
-SPARSE_OBS="--sparse_obs --sparse_rnd_tests $n_test_rnd --pr_sympt $pr_sympt --delay_test_p $delay_test_p"
+EXTRA_FLAGS=" --init_name_file $init_name --path_dir $path_dir "
+SPARSE_OBS="--sparse_obs --sparse_rnd_tests $n_test_rnd --pr_sympt $pr_sympt --delay_test_p $delay_test_p --sparse_obs_last"
 
 
 SIB_PARS="--p_source $p_source --maxit 1000 --p_sus $p_sus --sib_tol 1e-5"
@@ -81,5 +68,5 @@ for seed in $(seq 0 100)
 do
     echo $seed
     n_conf=$(( $st_conf+$num_conf ))
-    python $SCRIPT $GEN_GRAPH --seed $seed $EXTRA_FLAGS $SPARSE_OBS $SIB_PARS --nthreads 50 #$EXTRA_GEN
+    python $SCRIPT $GEN_GRAPH --seed $seed $EXTRA_FLAGS $SPARSE_OBS $SIB_PARS --num_conf $n_conf --start_conf $st_conf --nthreads 50 #$EXTRA_GEN
 done
